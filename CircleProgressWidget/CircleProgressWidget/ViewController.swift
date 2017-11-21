63fa31318f4ea5c6412a1399ecaad6f9332a20ad
Rosen Stoyanov <rosen.stoyanov@stanga.net>
2017-11-21 18:06:04 +0200

    init

---
+++ CircleProgressWidget/CircleProgressWidget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleProgressWidget
+//
+//  Created by Rosen Stoyanov on 21.11.17.
+//  Copyright © 2017 Rosen Stoyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
