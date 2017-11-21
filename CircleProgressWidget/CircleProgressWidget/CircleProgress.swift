6ca42f79be848ec5ba84eaa10343fa78e3f7154d
Rosen Stoyanov <rosen.stoyanov@stanga.net>
2017-11-21 18:19:23 +0200

    create Circle progect file

---
+++ CircleProgressWidget/CircleProgressWidget/CircleProgress.swift
@@ -0,0 +1,21 @@
+//
+//  CircleProgress.swift
+//  CircleProgressWidget
+//
+//  Created by Rosen Stoyanov on 21.11.17.
+//  Copyright © 2017 Rosen Stoyanov. All rights reserved.
+//
+
+import UIKit
+
+class CircleProgress: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
